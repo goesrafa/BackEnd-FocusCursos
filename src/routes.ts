3d061ddde69841fc524050ba036a491245e6049f
goesrafa <rafa.goes.2012@gmail.com>
2022-04-14 12:27:20 -0300

    Listagem e criação de categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,21 @@ import { Router } from "express";
 /*Importações criadas*/
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from "./controllers/user/AuthUserController";
-import {DetailUserController } from './controllers/user/DetailUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
 
-import {isAuthenticated} from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
 const router = Router();
 
 /**Rotas users**/
 router.post('/users', new CreateUserController().handle)
 router.post('/login', new AuthUserController().handle)
-router.get('/info',  isAuthenticated, new DetailUserController().handle)
+router.get('/info', isAuthenticated, new DetailUserController().handle)
+
+/*Rotas categories*/
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/categories', isAuthenticated, new ListCategoryController().handle )
 
 export { router }
