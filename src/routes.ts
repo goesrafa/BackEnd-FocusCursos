50430d1b5ed1386e61a4f47d11aaeb8217665e4c
goesrafa <rafa.goes.2012@gmail.com>
2022-04-14 11:33:43 -0300

    Implementação dos detalhes do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,14 @@ import { Router } from "express";
 /*Importações criadas*/
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import {DetailUserController } from './controllers/user/DetailUserController'
 
+import {isAuthenticated} from './middlewares/isAuthenticated'
 const router = Router();
 
 /**Rotas users**/
 router.post('/users', new CreateUserController().handle)
 router.post('/login', new AuthUserController().handle)
+router.get('/info',  isAuthenticated, new DetailUserController().handle)
 
 export { router }
